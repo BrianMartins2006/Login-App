8e45025af064158413d134b92de5f53a1ef338a7
GabrielSouzaCruz <contato.gabrielsouzacruz@gmail.com>
2026-04-07 10:11:09 -0300

    feat: EditText e Log

--- app/src/main/java/br/edu/ifsuldeminas/mch/login/MainActivity.java
+++ app/src/main/java/br/edu/ifsuldeminas/mch/login/MainActivity.java
@@ -1,8 +1,10 @@
 package br.edu.ifsuldeminas.mch.login;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +12,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String USER_NAME = "emerson";
+    private static final String PW = "admin";
+    private static final String LOG_TAG = "login_activity_main";
+
     private Button buttonLogin;
     private Button buttonRegister;
     private Button buttonForgotPW;
+    private EditText editTextUser;
+    private EditText editTextPW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +30,27 @@ public class MainActivity extends AppCompatActivity {
         buttonLogin = findViewById(R.id.buttonLoginId);
         buttonRegister = findViewById(R.id.buttonRegisterId);
         buttonForgotPW = findViewById(R.id.buttonForgotPWId);
+        editTextUser = findViewById(R.id.textInputLayoutUserId);
+        editTextPW = findViewById(R.id.textInputLayoutPWId);
+
 
         //interno
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast = Toast.makeText(getBaseContext(), "Botão Login Clicado!", Toast.LENGTH_LONG);
-                toast.show();
+
+                String userName = editTextUser.getText().toString();
+                String userPW = editTextPW.getText().toString();
+
+                if (USER_NAME.equals(userName) && PW.equals(userPW)){
+
+                }else {
+                    Toast toast = Toast.makeText(getBaseContext(), userName + " - " + userPW, Toast.LENGTH_LONG);
+                    toast.show();
+                    editTextUser.requestFocus();
+                }
+
+
             }
         });
 
@@ -39,6 +61,12 @@ public class MainActivity extends AppCompatActivity {
         buttonForgotPW.setOnClickListener((view) -> {
             Toast.makeText(view.getContext(), R.string.button_forgot_pw_clicked, Toast.LENGTH_LONG).show();
         });
-
+        Log.d(LOG_TAG, "Método onCreate da MainActivity executou com sucesso!");
         }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(LOG_TAG, "Activity foi fechada.");
     }
+}
