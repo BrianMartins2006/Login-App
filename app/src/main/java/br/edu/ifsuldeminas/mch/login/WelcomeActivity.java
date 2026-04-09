0890f08e5936d1ae40897f3b1a6e4f959450c85b
GabrielSouzaCruz <contato.gabrielsouzacruz@gmail.com>
2026-04-09 10:50:32 -0300

    feat: Alteraçoes WelcomeActivity, chamada da camera

--- app/src/main/java/br/edu/ifsuldeminas/mch/login/WelcomeActivity.java
+++ app/src/main/java/br/edu/ifsuldeminas/mch/login/WelcomeActivity.java
@@ -3,11 +3,16 @@ package br.edu.ifsuldeminas.mch.login;
 import static br.edu.ifsuldeminas.mch.login.R.id.activity_welcome_id;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -18,6 +23,10 @@ import com.google.android.material.snackbar.Snackbar;
 import java.text.MessageFormat;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private Button buttonTakePicture;
+    private ImageView imageViewPicture;
+    private static final int REQUEST_PICTURE_CODE = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +40,38 @@ public class WelcomeActivity extends AppCompatActivity {
 
         Intent intentQueChamou = getIntent();
         String userName = intentQueChamou.getStringExtra("user_name");
-
-        TextView textViewWelcome = findViewById(R.id.textViewWecolmeTextId);
-        String originalMessage = textViewWelcome.getText().toString();
-        String finalMessage = MessageFormat.format("{0} {1}!", originalMessage, userName);
-        textViewWelcome.setText(finalMessage);
+        String finalMessage = MessageFormat.format("{0} {1}!", getString(R.string.welcome_message), userName);
 
         View viewLayout = findViewById(activity_welcome_id);
         Snackbar snackbar = Snackbar.make(viewLayout, finalMessage, Snackbar.LENGTH_LONG);
         snackbar.show();
+
+        buttonTakePicture = findViewById(R.id.buttonTakePictureId);
+        imageViewPicture = findViewById(R.id.imageViewPictureId);
+
+        buttonTakePicture.setOnClickListener((View view) -> {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(takePictureIntent, REQUEST_PICTURE_CODE);
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_PICTURE_CODE){
+            if (data != null) {
+                Bundle extras = data.getExtras();
+                if (extras != null) {
+                    Bitmap image = (Bitmap) extras.get("data");
+                    imageViewPicture.setImageBitmap(image);
+                }
+            }
+        }
     }
 }
